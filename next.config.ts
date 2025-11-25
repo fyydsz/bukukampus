0254ac7fe1219c9b62147313f15c9504d0386896
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-11-25 09:01:17 +0700

    Update next.config.ts to integrate Nextra and configure turbopack aliases

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
+import nextra from "nextra";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    resolveAlias: {
+      'next-mdx-import-source-file': './mdx-components.tsx'
+    }
+  },
+  transpilePackages: ['shiki']
 };
 
-export default nextConfig;
+const withNextra = nextra({
+  contentDirBasePath: '/docs', // Or even nested e.g. `/docs/advanced`
+})
+export default withNextra(nextConfig);
