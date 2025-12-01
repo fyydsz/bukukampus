d94dd132cd3dbb5a54510bd290ed19299c6152e2
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-12-01 15:32:13 +0700

    Add custom code themes to Nextra config for syntax codes (Stranger Things)

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 import nextra from "nextra";
+import lightTheme from './public/syntax/light.json' assert { type: 'json' } // atau baca file manual
+import darkTheme from './public/syntax/dark.json' assert { type: 'json' }
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -16,6 +18,16 @@ const withNextra = nextra({
   latex: {
     renderer: 'katex', // 'katex' | 'mathjax'}
     options: {}
+  },
+  mdxOptions: {
+    rehypePrettyCodeOptions: {
+      theme: {
+        //@ts-expect-error // shiki theme type issue
+        light: lightTheme,
+        //@ts-expect-error // shiki theme type issue
+        dark: darkTheme
+      }
+    }
   }
 })
 export default withNextra(nextConfig);
