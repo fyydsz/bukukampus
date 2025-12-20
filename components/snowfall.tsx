cb9e85f48a535758a9a1a07656675135178d9745
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-12-20 18:17:00 +0700

    Respect reduced motion preference for snowfall

--- components/snowfall.tsx
+++ components/snowfall.tsx
@@ -29,25 +29,27 @@ const SnowfallWrapper: React.FC<SnowfallWrapperProps> = ({ children }) => {
   const prefersReducedMotion = useSyncExternalStore(
     subscribeToPrefersReducedMotion,
     getClientSnapshot,
-    getServerSnapshot
+    getServerSnapshot,
   );
 
   return (
     <div style={{ position: "relative", width: "100%", height: "100%" }}>
       {children}
-      <div
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100vw",
-          height: "100vh",
-          pointerEvents: "none",
-          zIndex: 9999,
-        }}
-      >
-        <Snowfall snowflakeCount={75} />
-      </div>
+      {!prefersReducedMotion && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            pointerEvents: "none",
+            zIndex: 9999,
+          }}
+        >
+          <Snowfall snowflakeCount={75} />
+        </div>
+      )}
     </div>
   );
 };
