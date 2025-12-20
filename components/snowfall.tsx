07aca68f6e753722631af0e2f45118a841577eea
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-12-20 13:51:13 +0700

    Add SnowfallWrapper component

---
+++ components/snowfall.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import Snowfall from "react-snowfall";
+
+interface SnowfallWrapperProps {
+  children: React.ReactNode;
+}
+
+const SnowfallWrapper: React.FC<SnowfallWrapperProps> = ({ children }) => {
+  return (
+    <div style={{ position: "relative", width: "100%", height: "100%" }}>
+      {children}
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          pointerEvents: "none",
+          zIndex: 9999,
+        }}
+      >
+        <Snowfall />
+      </div>
+    </div>
+  );
+};
+
+export default SnowfallWrapper;
